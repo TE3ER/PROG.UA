9ab547db21ba5a5d4646c66ecc7e178e17739cfc
Rost <6880416@stud.nau.edu.ua>
2023-10-30 14:01:31 +0300

    js1

---
+++ JS1/index.js
@@ -0,0 +1,16 @@
+let userNickname = 'TE3ER';
+console.log(userNickname)
+//////////////////////////////////////////////
+let userScore = 9;
+console.log(userScore)
+/////////////////////////////////////////////
+let priceFood = Number(prompt('Ціна продуктів'));
+let numberFood = Number(prompt('Кількість продуктів'));
+
+console.log(priceFood + " Grn");
+console.log(numberFood);
+let sum = priceFood*numberFood;
+console.log(sum+ " Grn");
+////////////////////////////////////////////
+sumProcent = sum*5/100;
+console.log(sumProcent + " Grn");
